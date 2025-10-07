d6855f326e4b7a86cc99625d698a2eaabd31a953
Fatemehkhanjanzadeh <fatemeh.kh7368@gmail.com>
2025-10-07 10:18:08 +0330

    Customize Contact model admin interface
    - Add list display, filtering, search and ordering options
    - Set date hierarchy and empty value display

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    empty_value_display='-empty-'
+    #fields = ["title",]
+    list_display = ["name","email","created_date"]
+    list_filter=("email",)
+    ordering=('created_date',)
+    search_fields=("email",) 
+
+admin.site.register(Contact,ContactAdmin)
